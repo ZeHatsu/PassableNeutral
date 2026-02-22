9f3e8674dff2272ede6f1718c058340c0319bfc6
ZeHatsu <58302207+ZeHatsu@users.noreply.github.com>
2026-02-22 22:27:55 +1300

    Fixed improper HLSL syntax

--- PBR_Neutral/passableNeutral.hlsl
+++ PBR_Neutral/passableNeutral.hlsl
@@ -1,7 +1,7 @@
 // Input color is non-negative and resides in the Linear Rec. 709 color space.
 // Output color is also Linear Rec. 709, but in the [0, 1] range.
 
-vec3 PassableNeutralDTF( vec3 color ) {
+float3 PassableNeutralDTF( float3 color ) {
 	const float startCompression = 0.8 - 0.04;
 	const float desaturation = .3;
 	static const float3x3 rgb2ok = {
